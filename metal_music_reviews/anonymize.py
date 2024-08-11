feb879a88476d97b3da32e9e80fc600801fc4693
jzh001 <jiangzhsg@gmail.com>
2024-08-11 23:28:46 +0800

    Removed comments

--- metal_music_reviews/anonymize.py
+++ metal_music_reviews/anonymize.py
@@ -5,10 +5,10 @@ def anonymize(path='reviews_small.csv', save_path='reviews_anonymized.csv', colu
     current_idx = 0
     for col in columns:
         name_to_idx = {item: i + current_idx for i, item in enumerate(df[col].unique())}
-        df[col] = df[col].apply(lambda x: name_to_idx[x])  # Store the transformed column back into the DataFrame
+        df[col] = df[col].apply(lambda x: name_to_idx[x])
         current_idx += len(name_to_idx)
     df.to_csv(save_path, index=False)
 
-if __name__ == '__main__':  # Corrected the condition
+if __name__ == '__main__': 
     print("Anonymizing Entries...")
     anonymize()
